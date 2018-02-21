ef98ef6a5ba6d778c4c5788fb4fb2887ddf1f498
Andrew Sempere <a@feralresearch.org>
2018-02-21 18:13:52 +0100

    End to end conversion process

--- _convert.sh
+++ _convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "      ...CONVERT: $1"
+echo " $1"
 node  _convert.js --src "$1" > "$1_converted.html"
--- _extract.sh
+++ _extract.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#set -x
+#trap read debug
 INFILE="$2"
 OUTPATH="$1/$(basename "$(dirname "$2")")"
 mkdir -p "$OUTPATH"
@@ -8,5 +10,10 @@ OUTFILE="$OUTPATH/$(basename "$INFILE").xml"
 # 2. strips the word-specific markup using sed
 # 3. The result is entity-encoded, so we use 'recode' to put the xml back together
 #echo "$OUTFILE";
-echo "      ...EXTRACT: $INFILE"
-unzip -p "$INFILE" | sed -e 's/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g' | recode html..utf8 > "$OUTFILE"
+echo " $INFILE"
+
+# Note: after the first strip, the rest is entity encoded.
+# I tried to use an entity decoder tool (recode) but it ate the encoding, causing the UTF8 accent chars to render wrong
+# In the end it was sufficient to replace the gt and lt, because only the HTML seemed encoded... except for ampersands which
+# were weirdly showing up as &amp;amp;
+unzip -p "$INFILE" word/document.xml | sed -e 's/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g' | sed -e 's/&gt;/>/g' | sed -e 's/&lt;/</g' | sed -e 's/amp;//g' > "$OUTFILE"
