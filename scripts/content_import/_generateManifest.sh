93d4050411135d0add042ad5e9c552f95f5024a7
Andrew Sempere <a@feralresearch.org>
2018-02-26 00:43:30 +0100

    Altering JSON manifest

--- scripts/content_import/_generateManifest.sh
+++ scripts/content_import/_generateManifest.sh
@@ -13,7 +13,10 @@ export RUNDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 FOLIO_DIRECTORY="$1"
 MANIFEST_FILE="$2"
-rm -rf $MANIFEST_FILE;
-echo "[" >> $MANIFEST_FILE
-find "$FOLIO_DIRECTORY" ! -path . -type d -mindepth 1 -maxdepth 1 -exec "$RUNDIR/_generateManifestEntryFor.sh" {} \; >> $MANIFEST_FILE
-echo "]" >> $MANIFEST_FILE
+rm -rf $MANIFEST_FILE
+find "$FOLIO_DIRECTORY" -mindepth 1 -maxdepth 1 ! -path . -type d  -exec "$RUNDIR/_generateManifestEntryFor.sh" {} \; >> "$MANIFEST_FILE.tmp"
+sed -i.bak '$!s/$/,/' "$MANIFEST_FILE.tmp"
+echo "{" > $MANIFEST_FILE
+cat "$MANIFEST_FILE.tmp" >> "$MANIFEST_FILE"
+echo "}" >> "$MANIFEST_FILE"
+rm -rf "$MANIFEST_FILE.tmp"
--- scripts/content_import/_generateManifestEntryFor.sh
+++ scripts/content_import/_generateManifestEntryFor.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 FILE="$1"
-printf "{\"folio\":\"%s\",\"last_update\":\"%s\"}," $(basename "$1") $(stat -f "%Sm" -t "%m%d%H%M%y" "$1")
+
+# Default (Darwin)
+if [ -z "$(uname -a | grep Ubuntu)" ]; then
+	printf "\"%s\":{\"folio\":\"%s\",\"last_update\":\"%s\"}\n" "$(basename "$1")" "$(basename "$1")" "$(stat -f "%Sm" -t "%m%d%H%M%y" "$1")"
+
+# Ubuntu
+else
+	printf "\"%s\":{\"folio\":\"%s\",\"last_update\":\"%s\"}\n" "$(basename "$1")" "$(basename "$1")" "$(stat -c %Y "$1")"
+fi
