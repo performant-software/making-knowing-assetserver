7c16cff5da8af2e7eda06bb957cd5e4dbae43227
Nick Laiacona <nick@performantsoftware.com>
2018-03-08 16:28:31 -0500

    script to add annotation lists to the bnf manifest

--- scripts/content_import/iiif_manifest.js
+++ scripts/content_import/iiif_manifest.js
@@ -5,6 +5,29 @@ const path = require('path');
 
 var domainName = "http://localhost:4000"
 
+
+function generateFolioID( bnfLabel ) {
+  // grab r or v off the end
+  let rectoOrVerso = bnfLabel.slice( bnfLabel.length-1 );
+  let id = parseInt(bnfLabel.slice(0,bnfLabel.length-1));
+
+  // the beginning and end pages do not have a numeric label
+  if( isNaN(id) ) return null;
+
+  // figure out how much padding we need
+  let zeros = "";
+
+  if( id < 10 ) {
+    zeros = zeros + "0"
+  }
+
+  if( id < 100 ) {
+    zeros = zeros + "0"
+  }
+
+  return `p${zeros.concat(id)}${rectoOrVerso}`;
+}
+
 // Add this to the manifest canvas entries:
 // "otherContent" : [ {
 //   "@id": "http://localhost:4000/bnf-ms-fr-640/list/p003r.json",
@@ -16,20 +39,26 @@ let manifest = JSON.parse(json);
 let canvases = manifest["sequences"][0]["canvases"];
 
 for( let canvas of canvases ) {
-  let canvasLabel = canvas["label"];
+  let folioID = generateFolioID(canvas["label"]);
 
-  // how many zeros?
-  // what about pages that aren't numbered?
+  if( folioID ) {
+    let annotationListURL =  `${domainName}/bnf-ms-fr-640/list/${folioID}.json`;
 
-  let folioID = `p00${canvasLabel}`;
-  let annotationListURL = `${domainName}/bnf-ms-fr-640/list/${folioID}.json`;
-
-  canvas["otherContent"] = [ {
-    "@id": annotationListURL,
-    "@type": "sc:AnnotationList"
-  } ];
+    canvas["otherContent"] = [ {
+      "@id": annotationListURL,
+      "@type": "sc:AnnotationList"
+    } ];
+  }
 
   // since we're here, shouldn't we also write out the annotation files?
 }
 
-console.log( manifest );
+fs.writeFile('TEMP/manifest.json', JSON.stringify(manifest, null, 3), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // success case, the file was saved
+      console.log('IIIF Manifest created.');
+    };
+
+});
