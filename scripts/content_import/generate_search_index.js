b04e082abb3e98d0749a77358f3463e601d33983
Nick Laiacona <nick@performantsoftware.com>
2018-03-27 15:48:32 -0400

    index a test file

--- scripts/content_import/generate_search_index.js
+++ scripts/content_import/generate_search_index.js
@@ -6,6 +6,26 @@ const webRoot = "TEMP/testindex";
 const folioDir = `${webRoot}/folio`;
 const searchIndexFile = `${webRoot}/searchIndex.js`;
 
+
+// returns transcription or null if unable to parse
+function parseTranscription(html) {
+
+  // pull transcription from folio element
+  let folioTag = "<folio";
+  let openDivIndex = html.indexOf(folioTag);
+  if (openDivIndex === -1) return null;
+  let start = html.indexOf(">", openDivIndex) + 1;
+  let end = html.lastIndexOf("</folio>");
+  if (end === -1) return null;
+  if (start >= end) return null;
+  let transcription = html.slice(start, end);
+
+  // drop tags in transcription
+  let content = transcription.replace(/<[^>]*>/g, '');
+
+  return content;
+}
+
 function main() {
 
   // make sure the folio dir exists
@@ -22,20 +42,23 @@ function main() {
     let folios = fs.readdirSync(folioDir);
     folios.forEach( folio => {
 
+      // ignore hidden directories
+      if( folio.startsWith('.') ) return;
+
       // get contents of folio file
       let tcHTML = fs.readFileSync(`${folioDir}/${folio}/tc/index.html`, "utf8");
 
+      // TODO
+      // pull html from each of the three subfolders
+      // index tc and tcn in french, tl in english
+
       // grab just the contents of folio element and then strip all tags from that
-      let folioContent =
+      let folioContent = parseTranscription(tcHTML);
 
       let folioDocument = {
         id: folio,
         content: folioContent
       }
-
-      // TODO
-      // pull html from each of the three subfolders
-      // index tc and tcn in french, tl in english
 
       this.add(folioDocument);
 
