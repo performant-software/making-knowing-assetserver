c656a0d7c13133ad039461ed6209cda53b2d7d18
Nick Laiacona <nick@performantsoftware.com>
2018-04-17 16:29:44 -0400

    context snippets for full text search

--- scripts/content_import/search_edition.js
+++ scripts/content_import/search_edition.js
@@ -12,37 +12,79 @@ const webRoot = "../../nginx/webroot";
 const searchIndexFile = `${webRoot}/search_index.js`;
 const recipeBookFile = `${webRoot}/recipe_book.js`;
 
-const MAX_FRAGMENT_LENGTH = 60;
+const MAX_FRAGMENT_LENGTH = 12;
+const HIGHLIGHT_START = '<span class="highlight">';
+const HIGHLIGHT_END = '</span>';
 
-function highlightFragment( contentFragment, termStart, termEnd ) {
-  // TODO add a highlight span
-  return contentFragment;
+function offsetToWordIndex( words, wordOffset ) {
+  var offset = 0;
+  for( let i=0; i < words.length; i++ ) {
+    if( offset === wordOffset ) {
+      return i;
+    }
+    offset = offset + words[i].length + 1;
+  }
+
+  // word not found
+  return -1;
+}
+
+function isPunctuation( character ) {
+  return (  character === '.' ||
+            character === '?' ||
+            character === '&' ||
+            character === '!' ||
+            character === ';' ||
+            character === ','     );
+}
+
+function createFragment( fullText, highlightPosition ) {
+  let contextFragment, checkEllipses;
+  let fragment = fullText.replace('\n', ' ').split(' ');
+  let termIndex = offsetToWordIndex( fragment, highlightPosition[0] );
+
+  // clip the fragment if the full text is too long too display
+  if( fragment.length > MAX_FRAGMENT_LENGTH ) {
+
+    // create a range that puts term in center of fragment
+    let fragStart = termIndex-Math.floor(MAX_FRAGMENT_LENGTH/2);
+    let fragEnd = fragStart + MAX_FRAGMENT_LENGTH;
+
+    // don't go past the ends of the string
+    fragStart = (fragStart < 0) ? 0 : fragStart;
+    fragEnd = (fragEnd > fragment.length) ? fragment.length : fragEnd;
+
+    // clip fragment and update term offsets
+    fragment = fragment.slice( fragStart, fragEnd );
+    termIndex = termIndex - fragStart;
+
+    checkEllipses = true;
+  }
+
+  // highlight the term
+  fragment[termIndex] = HIGHLIGHT_START + fragment[termIndex] + HIGHLIGHT_END;
+
+  // turn back into a string
+  contextFragment = fragment.join(' ');
+
+  // add ellipses if needed...
+  let lastLetter = contextFragment[contextFragment.length-1];
+  if( checkEllipses && !isPunctuation(lastLetter) ) {
+    contextFragment = contextFragment.concat('...');
+  }
+
+  return contextFragment;
 }
 
 // pull a window of text out of the content to display with the search result
-function contextFragments( resultMetadata, fullText ) {
+function createFragments( resultMetadata, fullText ) {
   let highlightedFragments = [];
 
   for( let keyword of Object.keys(resultMetadata) ) {
     let keywordData = resultMetadata[keyword];
     for( let highlightPosition of keywordData.content.position ) {
-      let highlightedFragment;
-      if( fullText.length > MAX_FRAGMENT_LENGTH ) {
-        let termStart = highlightPosition[0] ;
-        let termEnd = highlightPosition[1];
-        let termCenter = (termEnd-termStart)/2 + termStart;
-        let fragStart = termCenter-(MAX_FRAGMENT_LENGTH/2);
-        let fragEnd = fragStart + MAX_FRAGMENT_LENGTH;
-        fragStart = (fragStart < 0) ? 0 : fragStart;
-        fragEnd = (fragEnd > fullText.length) ? fullText.length : fragEnd;
-        let contentFragment = fullText.slice( fragStart, fragEnd );
-        termStart = termStart - fragStart;
-        termEnd = termEnd - fragStart;
-        highlightedFragment = highlightFragment( contentFragment, termStart, termEnd );
-      } else {
-        highlightedFragment = highlightFragment( fullText, ...highlightPosition );
-      }
-      highlightedFragments.push(highlightedFragment);
+      let fragment = createFragment( fullText, highlightPosition );
+      highlightedFragments.push(fragment);
     }
   }
 
@@ -66,11 +108,11 @@ function searchEdition( searchTerm ) {
 
   for( let result of results ) {
     let recipe = recipeBook[ result.ref ];
-    let fragments = contextFragments( result.matchData.metadata, recipe.content )
+    let fragments = createFragments( result.matchData.metadata, recipe.content )
     recipes.push( { name: recipe.name, folio: recipe.folioID, contextFragments: fragments } );
   }
 
   return recipes;
 }
 
-module.exports = searchEdition;
+module.exports = { searchEdition: searchEdition, offsetToWordIndex: offsetToWordIndex };
