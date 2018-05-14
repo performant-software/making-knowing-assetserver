a390c2a1b5c9bdfd575ebb187d2753f1e479cb63
Nick Laiacona <nick@performantsoftware.com>
2018-05-14 16:23:08 -0400

    record the id of the figure.

--- scripts/content_import/_convert.js
+++ scripts/content_import/_convert.js
@@ -135,9 +135,9 @@ for(var idx=0;idx<docparts.length;idx++){
 }
 
 // Strip all <figure> tags
-// Per https://github.com/cu-mkp/making-knowing-edition/issues/22, replace them with empty div
-xml = xml.replace(/<figure>.*<\/figure>/g, '<div></div>');
-
+// Per https://github.com/cu-mkp/making-knowing-edition/issues/22, replace them with div
+xml = xml.replace(/<figure><id>([^<]+)/g, '<div class="figure" id=\"$1\"></div><figure><id>');
+xml = xml.replace(/<figure>.*<\/figure>/g, '');
 
 // Beautify HTML
 if(f_beautify){
