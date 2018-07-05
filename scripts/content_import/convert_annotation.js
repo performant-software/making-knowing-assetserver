f8513e48aa310552f028625fba3899232a2b07e0
Nick Laiacona <nick@performantsoftware.com>
2018-07-05 09:27:38 -0400

    trying out different plugins to markdown-it

--- scripts/content_import/convert_annotation.js
+++ scripts/content_import/convert_annotation.js
@@ -1,20 +1,23 @@
 
 const fs = require('fs');
-var Markdown = require('markdown-it');
-   
+var Markdown = require('markdown-it')()
+// var { markdownItTable } = require('markdown-it-table');
 const inputFile = './TEMP/pan/annotest.md';
 const outputFile = './TEMP/pan/annotest.html';
 
 // use pandoc to convert from docx to md:
-// pandoc -f docx -t markdown annotest.docx
+// pandoc -f docx -t markdown annotest2.docx > annotest.md
 
 function main() {
+
+    Markdown.use(require('markdown-it-footnote'));
+    Markdown.use(require('markdown-it-multimd-table'));
+
     // read in a markdown file
     let mdAnnotation = fs.readFileSync(inputFile, "utf8");
 
     // process it to HTML
-    let md = new Markdown();
-    var result = md.render(mdAnnotation);
+    var result = Markdown.render(mdAnnotation);
 
     // write it back out
     fs.writeFileSync(outputFile, result );
