ee7aab6a26fd556fed0c48c9dc058c00481c0b6a
Nick Laiacona <nick@performantsoftware.com>
2018-07-17 08:27:06 -0400

    run every 20 mins on the hour

--- scripts/content_import/asset_server.js
+++ scripts/content_import/asset_server.js
@@ -1,17 +1,24 @@
 const searchIndex = require('./search_index');
 
+const waitTimeLengthMins = 20;
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function nextInterval() {
+    const now = new Date();
+    console.log( `time: ${now.toString()}`);
+    const nextIntervalMins = waitTimeLengthMins - (now.getMinutes() % waitTimeLengthMins);
+    const nextIntervalMs = (nextIntervalMins * 60) * 1000;
+    return nextIntervalMs;
+}
   
 async function main() {
-    const waitTimeMinutes = 20;
-    const waitTimeMs = (waitTimeMinutes * 60) * 1000;
-
     while(true) {
         console.log('Generating Search Index...');
         searchIndex.generate('../nginx/webroot');    
-        await sleep(waitTimeMs);
+        await sleep(nextInterval());
     }
 }
   
