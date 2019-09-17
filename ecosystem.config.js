d4f232b91e53a01fbc853a8c2a9c8d6435e7def1
Nick Laiacona <nick@performantsoftware.com>
2019-09-17 12:13:53 +0200

    fix typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,6 @@ module.exports = {
   apps : [{
     name      : 'asset_server',
     script    : 'making-knowing-edition/scripts/asset_server.js',
-    log       : 'ngimaking-knowing-edition/edition_data/working/pm2_logfile.txt'
+    log       : 'making-knowing-edition/edition_data/working/pm2_logfile.txt'
   }]
 };
