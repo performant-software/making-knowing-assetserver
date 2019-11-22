54feadf4b2f921a9d55018bf07ab79140a55140d
Nick Laiacona <nick@performantsoftware.com>
2019-11-22 18:20:33 -0500

    remove eco config

--- ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apps : [{
-    name      : 'asset_server',
-    script    : 'making-knowing-edition/scripts/asset_server.js',
-    log       : 'making-knowing-edition/edition_data/working/pm2_logfile.txt'
-  }]
-};
